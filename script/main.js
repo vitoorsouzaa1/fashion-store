0acad6014898d9d43d9ec644c0df46242a3c3812
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2022-02-22 13:51:59 -0300

    feat: add responsive navbar

---
+++ script/main.js
@@ -0,0 +1,9 @@
+const navList = document.querySelector('.nav-list');
+
+document.querySelector('.hamburger').onclick = () => {
+  navList.classList.add('show');
+};
+
+document.querySelector('.close').onclick = () => {
+  navList.classList.remove('show');
+};
